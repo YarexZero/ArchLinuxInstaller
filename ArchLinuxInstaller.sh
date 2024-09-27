f18d2feab095b4266ab7610cfcf04da25b1b9dde
Yarex793 <69676717+YarexZero@users.noreply.github.com>
2024-09-27 20:52:30 +0300

    ArchLinuxInstaller v1

---
+++ ArchLinuxInstaller.sh
@@ -0,0 +1,35 @@
+mkfs.ext4 /dev/sda2
+
+mkswap /dev/sda1
+
+mount /dev/sda2 /mnt
+
+swapon /dev/sda1
+
+pacstrap -K /mnt base linux linux-firmware php php-apache sudo vim mediawiki zerotier-one grub iwd
+
+locale-gen
+
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo "EndlessRPG-Server" > /etc/hostname
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+
+hwclock --systohc
+
+grub-install --target=i386-pc /dev/sda
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+passwd
+
+exit
+
+umount -R /mnt
+
+reboot
