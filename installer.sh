b767f4890d5729d16579382cb24b9924e2de57f8
Yarex793 <69676717+YarexZero@users.noreply.github.com>
2024-09-27 21:10:40 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-mkfs.ext4 /dev/sda2
-
-mkswap /dev/sda1
-
-mount /dev/sda2 /mnt
-
-swapon /dev/sda1
-
-pacstrap -K /mnt base linux linux-firmware php php-apache sudo vim mediawiki zerotier-one grub iwd
-
 locale-gen
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
