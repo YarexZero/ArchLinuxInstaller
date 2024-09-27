d2fd00307b619ab363ac7356f55d3b3b8ddaef70
Yarex793 <69676717+YarexZero@users.noreply.github.com>
2024-09-27 21:42:26 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-mkfs.ext4 /dev/sda2
-
-mkswap /dev/sda1
-
-mount /dev/sda2 /mnt
-
-swapon /dev/sda1
-
-pacstrap -K /mnt base linux linux-firmware php php-apache sudo vim mediawiki zerotier-one grub iwd b43-fwcutter b43-firmware
+pacstrap -K /mnt dhcpcd
 
 locale-gen
 
